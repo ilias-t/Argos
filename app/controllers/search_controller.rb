b42db313d2c1404eac74810dfcfa9eb16b20c337
kdoyne <kdoyne930@gmail.com>
2014-05-03 17:19:05 -0400

    searching

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,33 +1,15 @@
 class SearchController < ApplicationController
 
-  # def index
-  #   if params[:type] == "person"
-  #     @query = People.where(:name == params[:query])
-  #   elsif params[:type] == "organization"
-  #     @query = Organization.where(:name == params[:query])
-  #   end
-  #   @results = search(@query, params[:type])
-
-  #   render :JSON
-
-  # end
-
-  def show
-    if params[:type] == "person"
-      @query = People.where(:name == params[:query])
-    elsif params[:type] == "organization"
-      @query = Organization.where(:name == params[:query])
-    end
+  def index
+    @query = Organization.find_by_name(params[:query].downcase)
     @results = search(@query.crunchbase_id, params[:type])
     render :show
   end
 
 private
 
-  def search(query, type)
-    HTTParty.get("http://api.crunchbase.com/v/2/#{type}/#{query}?user_key=#{CRUNCHBASE_API_KEY}")
+  def search(query)
+    HTTParty.get("http://api.crunchbase.com/v/2/organization/#{query}?user_key=#{CRUNCHBASE_API_KEY}")
   end
-
-
 
 end
