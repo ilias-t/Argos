012cce766397b6eb75a9c088ac36cdf8de926218
kdoyne <kdoyne930@gmail.com>
2014-05-03 16:09:41 -0400

    setting up basic calls and search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,22 +1,34 @@
 class SearchController < ApplicationController
 
-  def index
-    results = search(params[:query], params[:type])
+  # def index
+  #   if params[:type] == "person"
+  #     @query = People.where(:name == params[:query])
+  #   elsif params[:type] == "organization"
+  #     @query = Organization.where(:name == params[:query])
+  #   end
+  #   @results = search(@query, params[:type])
 
+  #   render :JSON
 
-    render :JSON
-
-  end
+  # end
 
   def show
+    if params[:type] == "person"
+      @query = People.where(:name == params[:query])
+    elsif params[:type] == "organization"
+      @query = Organization.where(:name == params[:query])
+    end
+    @results = search(@query, params[:type])
     render :show
   end
 
 private
 
   def search(query, type)
-    HTTParty.get("http://api.crunchbase.com/v/2/#{type}/#{query}?user_key=#{CRUNCHBASE_API_KEY}")
+    query_format = query.gsub(" ", "-")
+    HTTParty.get("http://api.crunchbase.com/v/2/#{type}/#{query_format}?user_key=#{CRUNCHBASE_API_KEY}")
   end
+
 
 
 end
