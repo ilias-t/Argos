50249772c38b97f9efdbffec2d795d80ee7f0aa5
iliastsangaris <iliastsangaris@gmail.com>
2014-05-03 18:09:49 -0400

    Helper Modules for Crunchbase API

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,25 @@
 module ApplicationHelper
+
+# CrunchBase API helper methods
+  def getCompanyName(response)
+    return response["data"]["properties"]["name"]
+  end
+
+  def getCompanyDescription(response)
+    return response["data"]["properties"]["description"]
+  end
+
+  def getPrimaryRole(response)
+    return response["data"]["properties"]["primary_role"]
+  end
+
+  def getFoundingDate(response)
+    month = response["data"]["properties"]["founded_on_month"]
+    day = response["data"]["properties"]["founded_on_day"]
+    year = response["data"]["properties"]["founded_on_year"]
+    founded_date = month.to_s + "/" + day.to_s + "/" + year.to_s
+    return founded_date 
+  end
+
+
 end
