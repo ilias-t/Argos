ccdc28f3a422a7a117445f7be5f317ce94d863d9
kdoyne <kdoyne930@gmail.com>
2014-05-03 15:37:16 -0400

    set up env vars

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,22 @@
 class SearchController < ApplicationController
 
+  def index
+    results = search(params[:query], params[:type])
+
+
+    render :JSON
+
+  end
+
   def show
     render :show
   end
-  
+
+  private
+
+  def search(query, type)
+    HTTParty.get = ""
+  end
+
+
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,8 @@
 class WelcomeController < ApplicationController
+require 'pry'
 
   def index
+    binding.pry
     render :index
   end
 
