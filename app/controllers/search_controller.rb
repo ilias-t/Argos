cb0a4e400852abe3090a53406c87e5c063c7ff08
kdoyne <kdoyne930@gmail.com>
2014-05-03 18:26:11 -0400

    fixed small error

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,7 @@
 class SearchController < ApplicationController
 
-
   def index
-    @query = Organization.find_by_name(params[:query].downcase)
+    @query = Organizations.find_by_name(params[:query].downcase)
     @results = search(@query.crunchbase_id, params[:type])
     render :index
   end
