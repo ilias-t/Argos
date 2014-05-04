d184ce9f7e98e3c1c443787b34740d022336c189
Tahira <tahira.adaya@gmail.com>
2014-05-03 22:34:33 -0400

    fix map styling

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,13 +1,8 @@
 class SearchController < ApplicationController
 
   def index
-<<<<<<< HEAD
-    # @query = Organizations.find_by_name(params[:query].downcase)
-    # @results = search(@query.crunchbase_id)
-=======
     @query = Organizations.find_by_name(params[:query].downcase)
     @results = search(@query.crunchbase_id)
->>>>>>> 01311c6a3277e718d9f888149020551b30762cfe
     render :index
   end
 
